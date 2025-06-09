a4b491692bb980e194138ab6a3c92bb7541758ff
thakrisp <61070282+thakrisp@users.noreply.github.com>
2025-06-08 20:22:42 -0400

    added testing and updated ci.yml

---
+++ src/tests/auth.test.ts
@@ -0,0 +1,35 @@
+import { randomBytes } from "crypto";
+import type { IncomingHttpHeaders } from "http2";
+import { getAPIKey } from "src/api/auth";
+import { describe, expect, test } from "vitest";
+
+describe("Test getAPI key", () => {
+  const validKey = randomBytes(32).toString("hex");
+
+  test("valid key", () => {
+    const headers: IncomingHttpHeaders = {
+      authorization: `ApiKey ${validKey}`
+    }
+
+    const key = getAPIKey(headers)
+
+    expect(key).toEqual(validKey);
+  })
+
+  test("invalid key", () => {
+    const headers: IncomingHttpHeaders = {
+      authorization: `BEARER ${validKey}`
+    }
+
+    const key = getAPIKey(headers)
+
+    expect(key).toEqual(validKey);
+  })
+
+  test("invalid header", () => {
+    const headers: IncomingHttpHeaders = {}
+    const key = getAPIKey(headers)
+
+    expect(key).toEqual(null);
+  })
+})
