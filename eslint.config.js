443a7cfdeccd4b637c5d1ee6d31e5f0f8cb997e9
thakrisp <61070282+thakrisp@users.noreply.github.com>
2025-06-08 21:25:55 -0400

    added security to eslint config and ci workflow

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,15 @@
-import js from "@eslint/js";
 import globals from "globals";
+import { globalIgnores } from "eslint/config"
+import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-import { defineConfig, globalIgnores } from "eslint/config";
+import pluginSecurity from "eslint-plugin-security";
 
-
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
-  [globalIgnores(["./dist/"])]
-]);
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginSecurity.configs.recommended,
+  globalIgnores(["./dist/"])
+];
